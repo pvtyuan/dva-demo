bc226a4669d7fddf1467069d2eba80a78561edc9
pvtyuan <pvtyuan@foxmail.com>
2017-01-16 23:18:16 +0800

    add header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Menu, Icon, Popover} from "antd";
+import styles from "./Header.css"
+import Menus from "./Menus"
+
+const SubMenu = Menu.SubMenu;
+
+const Header = ({}) => {
+  const menusProps = {
+    isNavbar,
+  }
+
+  return (
+    <div className={styles.normal}>
+
+    </div>
+  );
+}
